597ee6eb6231c0e472598f63d298849f7ed1c228
UjjwalZambad <ujjwalzambad2003@gmail.com>
2025-09-26 21:11:25 +0530

    task 2

---
+++ Task2/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Task2.Models
+    ;
+namespace Task2.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        
+        public DbSet<Employee> Employees { get; set; }
+
+        public async Task<List<Employee>> GetEmployeesWithSalaryAboveAsync(decimal minSalary)
+        {
+            return await Employees
+                .FromSqlRaw("EXEC GetEmployeesWithSalaryAbove @MinSalary={0}", minSalary)
+                .ToListAsync();
+        }
+
+    }
+
+}
---
+++ Task2/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Task2.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId   { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public string Department { get; set; }
+    }
+}
